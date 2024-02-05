c719bd87a95b9ed0249479c0aa0824131da4e739
giovanigomez1 <giovani-gomez1@hotmail.com>
2024-02-05 07:47:04 -0600

    Js script correction

--- dist/index.9345d665.js
+++ dist/index.9345d665.js
@@ -1,8 +1,6 @@
 const overlay = document.querySelector(".overlay");
 const nav = document.querySelector(".nav");
 const nav__menu = nav.querySelector(".nav__menu");
-const menuOpen = nav.querySelector(".nav__btn");
-const menuClose = nav.querySelector(".nav__menu--close_btn");
 nav.addEventListener("click", function(e) {
     if (e.target.closest(".nav__btn")) {
         overlay.classList.remove("hidden");
